9ca33115b78a658129a969c15f9eed767fe3ef85
Salvatore Gabriele La Greca <gabriele.lagreca@gmail.com>
2020-11-04 00:30:19 +0100

    Added support for timer expire more then 65k ms. Display expires now after 180 sec

--- TrampolineOS/projects/os4es_assignment_exam/callback.c
+++ TrampolineOS/projects/os4es_assignment_exam/callback.c
@@ -2,8 +2,12 @@
 #include "tpl_os.h"
 #include "tpl_app_config.h"
 
-extern bool is_time_expired; /* defined in task.cpp */
+extern uint8_t expired_time_counter; /* defined in task.cpp */
+extern uint8_t counter_multiplier;	 /* defined in task.cpp */
+extern uint8_t MORSE_LED;            /* defined in code.cpp */
 
 ALARMCALLBACK(ALARMStopDisplay) {
-        is_time_expired = true;
+	if ((++expired_time_counter) == counter_multiplier) {
+		digitalWrite(MORSE_LED, 0x00);
+	}
 };
--- TrampolineOS/projects/os4es_assignment_exam/task.cpp
+++ TrampolineOS/projects/os4es_assignment_exam/task.cpp
@@ -8,7 +8,8 @@
 #define MORSE_CODING_BUFFER_LEN 	0x0004
 #define BIT_TIMING_MS				0x0064
 #define INTERMESSAGE_PAUSE_MS		0x01f4
-#define MAX_MSG_LENGTH_MS			0x7530
+#define COUNTER_TIMEXP_MAXVAL		0x0003
+#define MAX_MSG_LENGTH_MS			0xea60
 
 /** MACRO **/
 #define SEND_MULTIPLE_BITS_WEVENTS(bits, pin, times) 			 \
@@ -23,9 +24,10 @@
 
 #define SEND_INTERWORD_PAUSE(pin) SEND_MULTIPLE_BITS_WEVENTS(0x00, pin, 4)
 #define SEND_INTERCODEWORD_PAUSE(pin) SEND_MULTIPLE_BITS_WEVENTS(0x00, pin, 3)
+#define IS_TIME_EXPIRED(counter) (counter == counter_multiplier)
 
 /** CONST GLOBAL VARS **/
-extern uint8_t MORSE_LED;
+extern uint8_t MORSE_LED; /* defined in code.cpp */
 static char *PREDEF_MSGS[PREDEF_MSGS_ARRAY_LEN] = {
 	"A FEATHER IN THE HAND IS BETTER THAN A BIRD IN THE AIR",
 	"A SHORT PENCIL IS USUALLY BETTER THAN A LONG MEMORY ANY DAY",
@@ -35,15 +37,16 @@ static char *PREDEF_MSGS[PREDEF_MSGS_ARRAY_LEN] = {
 };
 
 /** GLOBAL VARS	 **/
+volatile uint8_t counter_multiplier = COUNTER_TIMEXP_MAXVAL;
+volatile uint8_t expired_time_counter;
 static uint8_t msg_index = 0x00;
-volatile bool is_time_expired;
 
 TASK(TaskTwitterer) {
 	long total_time;
 
 	do {
-		is_time_expired = false;
-		SetRelAlarm(ALARMStopDisplay, MAX_MSG_LENGTH_MS, 0x0);
+		expired_time_counter = 0x00;
+		SetRelAlarm(ALARMStopDisplay, MAX_MSG_LENGTH_MS, MAX_MSG_LENGTH_MS);
 
 		do {
 			Serial.print("Sending msg #");
@@ -57,11 +60,13 @@ TASK(TaskTwitterer) {
 			Serial.print(" - Elapsed: ");
 			Serial.println(total_time);
 
-		} while (ClearEvent(EVTMsgSendCompleted), !is_time_expired);
+		} while (ClearEvent(EVTMsgSendCompleted), !IS_TIME_EXPIRED(expired_time_counter));
 
 		SetRelAlarm(ALARMMsgPause, INTERMESSAGE_PAUSE_MS, 0x0);
-		msg_index = (msg_index + 1) % PREDEF_MSGS_ARRAY_LEN;
 		
+		msg_index = (msg_index + 1) % PREDEF_MSGS_ARRAY_LEN;
+		CancelAlarm(ALARMStopDisplay);
+
 		WaitEvent(EVTMsgPause);
 		ClearEvent(EVTMsgPause);
 	} while(true);
@@ -74,7 +79,7 @@ TASK(TaskSender) {
 	uint8_t i;
 	char *msg_ch_ptr;
 
-	for (msg_ch_ptr = PREDEF_MSGS[msg_index]; !is_time_expired && *msg_ch_ptr; msg_ch_ptr++) {
+	for (msg_ch_ptr = PREDEF_MSGS[msg_index]; !IS_TIME_EXPIRED(expired_time_counter) && *msg_ch_ptr; msg_ch_ptr++) {
 
 		if (*msg_ch_ptr == ' ') {
 			SEND_INTERWORD_PAUSE(MORSE_LED);
@@ -82,17 +87,17 @@ TASK(TaskSender) {
 		}
 		
 		char2morse(*msg_ch_ptr, coded_char_buff);
-		for (i = 0; i < MORSE_CODING_BUFFER_LEN && coded_char_buff[i] != NILL && !is_time_expired; i++) {
+		for (i = 0; i < MORSE_CODING_BUFFER_LEN && coded_char_buff[i] != NILL && !IS_TIME_EXPIRED(expired_time_counter); i++) {
 			SetRelAlarm(ALARMBitTiming, BIT_TIMING_MS, BIT_TIMING_MS);
 			send_bits(coded_char_buff[i], MORSE_LED);
-			ACTION_WVALID_FLAG(!is_time_expired, send_bits(0x00, MORSE_LED));
+			ACTION_WVALID_FLAG(!IS_TIME_EXPIRED(expired_time_counter), send_bits(0x00, MORSE_LED));
 			CancelAlarm(ALARMBitTiming);
 		}
 
-		ACTION_WVALID_FLAG(!is_time_expired, SEND_INTERCODEWORD_PAUSE(MORSE_LED));
+		ACTION_WVALID_FLAG(!IS_TIME_EXPIRED(expired_time_counter), SEND_INTERCODEWORD_PAUSE(MORSE_LED));
 	}
 
-	ACTION_WVALID_FLAG(!is_time_expired, SEND_INTERWORD_PAUSE(MORSE_LED));
+	ACTION_WVALID_FLAG(!IS_TIME_EXPIRED(expired_time_counter), SEND_INTERWORD_PAUSE(MORSE_LED));
 	SetEvent(TaskTwitterer, EVTMsgSendCompleted);
 
 	TerminateTask();
