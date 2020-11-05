6cf80c514fb038e8d3f68d336b6865ee3d400898
Salvatore Gabriele La Greca <gabriele.lagreca@gmail.com>
2020-11-05 12:52:12 +0100

    Enlarged counter size. Timing fix. Code clean.

---
+++ TrampolineOS/projects/os4es_assignment_exam/callback.cpp
@@ -0,0 +1,26 @@
+#include "Arduino.h"
+#include "tpl_os.h"
+#include "tpl_app_config.h"
+
+/*
+ * The trampoline definition of this macro doesn't work
+ * with my cpp compiler :( 
+ * But I think they forgot a ";" after the definition of
+ * the function prototype, so let's redefine it correctly!
+*/
+#ifdef ALARMCALLBACK
+#undef ALARMCALLBACK
+
+#define ALARMCALLBACK(name)  					   \
+			extern "C" void name##_callback(void); \
+			void name##_callback(void)
+
+#endif
+
+extern bool is_time_expired;
+extern uint8_t MORSE_LED;            /* defined in code.cpp */
+
+ALARMCALLBACK(ALARMStopDisplay) {
+	is_time_expired = true;
+	digitalWrite(MORSE_LED, 0x00);
+}
--- TrampolineOS/projects/os4es_assignment_exam/task.cpp
+++ TrampolineOS/projects/os4es_assignment_exam/task.cpp
@@ -4,12 +4,41 @@
 #include "comm_fnct.h"
 
 /** CONSTS **/
+/*
+ *	The size of a predefined messages array.
+ *	Default value is 5
+*/
 #define PREDEF_MSGS_ARRAY_LEN 		0x0005
+
+/*
+ *	The buffer size of a morse encoded character.
+ *	Default value is 4
+*/
 #define MORSE_CODING_BUFFER_LEN 	0x0004
-#define BIT_TIMING_MS				0x0064
-#define INTERMESSAGE_PAUSE_MS		0x01f4
-#define COUNTER_TIMEXP_MAXVAL		0x0003
-#define MAX_MSG_LENGTH_MS			0xea60
+
+/*
+ *	The delay between a bit and another.
+ *	
+ *	PS: Please note that value 1000 equals to 1024 ms, 
+ *		so 1 second is less than value 1000
+*/
+#define BIT_TIMING_MS				0x0062
+
+/*
+ *	How much time the display must stay off before starting a new message
+ *	
+ * 	PS: Please note that value 1000 equals to 1024 ms, 
+ *		so 1 second is less than value 1000
+*/
+#define INTERMESSAGE_PAUSE_MS		0x01e8
+
+/*
+ *	How much time the display can be used before interrupted.
+ *	
+ *	PS: Please note that value 1000 equals to 1024 ms, 
+ *		so 1 second is less than value 1000
+*/
+#define MAX_MSG_LENGTH_MS			0x2aea4
 
 /** MACRO **/
 #define SEND_MULTIPLE_BITS_WEVENTS(bits, pin, times) 			 \
@@ -24,29 +53,27 @@
 
 #define SEND_INTERWORD_PAUSE(pin) SEND_MULTIPLE_BITS_WEVENTS(0x00, pin, 4)
 #define SEND_INTERCODEWORD_PAUSE(pin) SEND_MULTIPLE_BITS_WEVENTS(0x00, pin, 3)
-#define IS_TIME_EXPIRED(counter) (counter == counter_multiplier)
 
 /** CONST GLOBAL VARS **/
 extern uint8_t MORSE_LED; /* defined in code.cpp */
 static char *PREDEF_MSGS[PREDEF_MSGS_ARRAY_LEN] = {
-	"A FEATHER IN THE HAND IS BETTER THAN A BIRD IN THE AIR",
-	"A SHORT PENCIL IS USUALLY BETTER THAN A LONG MEMORY ANY DAY",
-	"ACCEPT SOMETHING THAT YOU CANNOT CHANGE AND YOU WILL FEEL BETTER",
-	"ADVENTURE CAN BE REAL HAPPINESS",
-	"ALL THE EFFORT YOU ARE MAKING WILL ULTIMATELY PAY OFF"
+	(char *)"A FEATHER IN THE HAND IS BETTER THAN A BIRD IN THE AIR",
+	(char *)"A SHORT PENCIL IS USUALLY BETTER THAN A LONG MEMORY ANY DAY",
+	(char *)"ACCEPT SOMETHING THAT YOU CANNOT CHANGE AND YOU WILL FEEL BETTER",
+	(char *)"ADVENTURE CAN BE REAL HAPPINESS",
+	(char *)"ALL THE EFFORT YOU ARE MAKING WILL ULTIMATELY PAY OFF"
 };
 
 /** GLOBAL VARS	 **/
-volatile uint8_t counter_multiplier = COUNTER_TIMEXP_MAXVAL;
-volatile uint8_t expired_time_counter;
+volatile bool is_time_expired;
 static uint8_t msg_index = 0x00;
 
 TASK(TaskTwitterer) {
 	long total_time;
 
 	do {
-		expired_time_counter = 0x00;
-		SetRelAlarm(ALARMStopDisplay, MAX_MSG_LENGTH_MS, MAX_MSG_LENGTH_MS);
+		is_time_expired = false;
+		SetRelAlarm(ALARMStopDisplay, MAX_MSG_LENGTH_MS, 0x00);
 
 		do {
 			Serial.print("Sending msg #");
@@ -60,12 +87,10 @@ TASK(TaskTwitterer) {
 			Serial.print(" - Elapsed: ");
 			Serial.println(total_time);
 
-		} while (ClearEvent(EVTMsgSendCompleted), !IS_TIME_EXPIRED(expired_time_counter));
+		} while (ClearEvent(EVTMsgSendCompleted), !is_time_expired);
 
 		SetRelAlarm(ALARMMsgPause, INTERMESSAGE_PAUSE_MS, 0x0);
-		
 		msg_index = (msg_index + 1) % PREDEF_MSGS_ARRAY_LEN;
-		CancelAlarm(ALARMStopDisplay);
 
 		WaitEvent(EVTMsgPause);
 		ClearEvent(EVTMsgPause);
@@ -78,26 +103,31 @@ TASK(TaskSender) {
 	morse_t coded_char_buff[MORSE_CODING_BUFFER_LEN];
 	uint8_t i;
 	char *msg_ch_ptr;
+	long total_time;
 
-	for (msg_ch_ptr = PREDEF_MSGS[msg_index]; !IS_TIME_EXPIRED(expired_time_counter) && *msg_ch_ptr; msg_ch_ptr++) {
+	for (msg_ch_ptr = PREDEF_MSGS[msg_index]; !is_time_expired && *msg_ch_ptr; msg_ch_ptr++) {
 
 		if (*msg_ch_ptr == ' ') {
 			SEND_INTERWORD_PAUSE(MORSE_LED);
 			continue;
 		}
 		
+		//total_time = millis();
 		char2morse(*msg_ch_ptr, coded_char_buff);
-		for (i = 0; i < MORSE_CODING_BUFFER_LEN && coded_char_buff[i] != NILL && !IS_TIME_EXPIRED(expired_time_counter); i++) {
+		//Serial.print("char2morse: ");
+		//Serial.println(millis() - total_time);
+
+		for (i = 0; i < MORSE_CODING_BUFFER_LEN && coded_char_buff[i] != NILL && !is_time_expired; i++) {
 			SetRelAlarm(ALARMBitTiming, BIT_TIMING_MS, BIT_TIMING_MS);
 			send_bits(coded_char_buff[i], MORSE_LED);
-			ACTION_WVALID_FLAG(!IS_TIME_EXPIRED(expired_time_counter), send_bits(0x00, MORSE_LED));
+			ACTION_WVALID_FLAG(!is_time_expired, send_bits(0x00, MORSE_LED));
 			CancelAlarm(ALARMBitTiming);
 		}
 
-		ACTION_WVALID_FLAG(!IS_TIME_EXPIRED(expired_time_counter), SEND_INTERCODEWORD_PAUSE(MORSE_LED));
+		ACTION_WVALID_FLAG(!is_time_expired, SEND_INTERCODEWORD_PAUSE(MORSE_LED));
 	}
 
-	ACTION_WVALID_FLAG(!IS_TIME_EXPIRED(expired_time_counter), SEND_INTERWORD_PAUSE(MORSE_LED));
+	ACTION_WVALID_FLAG(!is_time_expired, SEND_INTERWORD_PAUSE(MORSE_LED));
 	SetEvent(TaskTwitterer, EVTMsgSendCompleted);
 
 	TerminateTask();
