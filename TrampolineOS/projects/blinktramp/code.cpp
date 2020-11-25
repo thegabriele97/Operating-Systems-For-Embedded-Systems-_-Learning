a815f3a6137e075c1428905aee50485582d83985
Salvatore Gabriele La Greca <gabriele.lagreca@gmail.com>
2020-11-25 18:18:44 +0100

    simple basic example

--- TrampolineOS/projects/blinktramp/code.cpp
+++ TrampolineOS/projects/blinktramp/code.cpp
@@ -5,8 +5,7 @@
 
 void setup()
 {
-	// initialize digital pin 13 as an output.
-	pinMode(13, OUTPUT);
+
 }
 
 //The TASK is activated by the alarm "periodicAl":
@@ -16,9 +15,6 @@ void setup()
 //* This task is periodic, with a period of 1024ms.
 TASK(periodicTask)
 {
-	static unsigned int nb = 0;
-	nb++;
-	if(nb & 1) digitalWrite(13, HIGH); //odd
-	else digitalWrite(13, LOW);        //even
+	
 }
 
